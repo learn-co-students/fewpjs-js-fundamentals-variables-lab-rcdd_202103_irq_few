8caf4334ff45fa39cb2ce04d6de3958322be1e99
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-04-02 16:35:14 +0000

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,9 +1,8 @@
-const fs = require('file-system')
-const jsdom = require('mocha-jsdom')
-const path = require('path')
+const fs = require('file-system');
+const jsdom = require('mocha-jsdom');
+const path = require('path');
 const babel = require('babel-core');
-
-const js = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
+const js = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8');
 
 describe('index.js', function () {
   describe('companyName', function () {
